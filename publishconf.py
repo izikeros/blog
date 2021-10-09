187fc44fcd64b3ffef2ebeb6ac0a9d1150f4eebb
Krystian Safjan <ksafjan@gmail.com>
2021-10-10 00:07:17 +0200

    Change cname and domain

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://safjan.com'
+SITEURL = 'https://www.safjan.com'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
